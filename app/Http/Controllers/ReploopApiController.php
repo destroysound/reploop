68e05687f8f66b1c074e085197ae24317c2e7e29
Willow Hunt <whunt.it@gmail.com>
2015-10-12 16:16:40 -0400

    Add pagination to proxied endpoint.

--- app/Http/Controllers/ReploopApiController.php
+++ app/Http/Controllers/ReploopApiController.php
@@ -16,7 +16,9 @@ class ReploopApiController extends Controller
     public function index()
     {
        $client = new \GuzzleHttp\Client();
-       $res = $client->request('GET', 'http://test.localfeedbackloop.com/api?apiKey=61067f81f8cf7e4a1f673cd230216112&noOfReviews=10&internal=1&yelp=1&google=1&offset=50&threshold=1', []); 
+       // Calculate offset based on 10 items per page.
+       $offset = Input::get('page', 0) * 10;
+       $res = $client->request('GET', 'http://test.localfeedbackloop.com/api?apiKey=61067f81f8cf7e4a1f673cd230216112&noOfReviews=10&internal=1&yelp=1&google=1&offset='.$offset.'&threshold=1', []); 
        return response()->json(json_decode($res->getBody()));
     }
 
