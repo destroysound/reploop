4859efd9fd7127a0bcaf628a0ebf6e4db63c06b6
Willow Hunt <whunt.it@gmail.com>
2015-10-12 17:53:15 -0400

    Fix bindings.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,34 +8,36 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc= sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
         <script src="http://ajax.aspnetcdn.com/ajax/knockout/knockout-3.1.0.js"></script>
         <script type="text/javascript">
-                var boundData = {};
-                function getPage(page) {
-                    $.get("/api", {'page': page}, function (data) {
-                        data.page = page;
-                        data.no_of_pages = Math.ceil(data.business_info.total_rating.total_no_of_reviews/10)
-                        console.log(data);
-                        data.paginator = ko.computed(function() {
-                            var begin_page = page - 3;
-                            if (begin_page < 0) {
-                                begin_page = 0;
-                            }
-                            end_page = begin_page + 6;
-                            if (end_page > data.no_of_pages) {
-                                end_page = data.no_of_pages;
-                            }
-                            var arr = [];
-                            for (var i = begin_page; i < end_page; i++) {
-                                arr.push(i);
-                            }
-                            return arr;
-                        });
-                        boundData = data;
-                    });
-                }
-            $(function () {
-                getPage(0);
-                ko.applyBindings(boundData);
-            });
+          var boundData = {
+            'page': 0,
+            'reviews': []
+          };
+          boundDta.paginator = ko.computed(function() {
+	    var begin_page = page - 3;
+	    if (begin_page < 0) {
+		begin_page = 0;
+	    }
+	    end_page = begin_page + 6;
+	    if (end_page > data.no_of_pages) {
+		end_page = data.no_of_pages;
+	    }
+	    var arr = [];
+	    for (var i = begin_page; i < end_page; i++) {
+		arr.push(i);
+	    }
+	    return arr;
+	  });
+          function getPage(page) {
+	    $.get("/api", {'page': page}, function (data) {
+                boundData.page = page;
+                boundData.no_of_pages = Math.ceil(data.business_info.total_rating.total_no_of_reviews/10)
+                boundData.reviews = data.reviews;
+	    });
+          }
+          $(function () {
+            getPage(0);
+            ko.applyBindings(boundData);
+          });
         </script>
     </head>
     <body>
