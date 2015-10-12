066392ed0518d707e05a82d3ea95671f7f081f15
Willow Hunt <whunt.it@gmail.com>
2015-10-12 16:24:30 -0400

    Add number of pages to object.

--- app/Http/Controllers/ReploopApiController.php
+++ app/Http/Controllers/ReploopApiController.php
@@ -19,7 +19,10 @@ class ReploopApiController extends Controller
        // Calculate offset based on 10 items per page.
        $offset = $request->input('page', 0) * 10;
        $res = $client->request('GET', 'http://test.localfeedbackloop.com/api?apiKey=61067f81f8cf7e4a1f673cd230216112&noOfReviews=10&internal=1&yelp=1&google=1&offset='.$offset.'&threshold=1', []); 
-       return response()->json(json_decode($res->getBody()));
+       $results = json_decode($res->getBody());
+       $results['business_info']['total_rating']['no_of_pages'] =
+           ceil($results['business_info']['total_rating']['no_of_reviews'] / 10)
+       return response()->json($results);
     }
 
 }
