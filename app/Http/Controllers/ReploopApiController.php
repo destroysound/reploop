fd35a3bcbfac8ef1fa8a56179ec2ecc5da841e5d
Willow Hunt <whunt.it@gmail.com>
2015-10-12 16:14:04 -0400

    Fix guzzle statement.

--- app/Http/Controllers/ReploopApiController.php
+++ app/Http/Controllers/ReploopApiController.php
@@ -17,7 +17,7 @@ class ReploopApiController extends Controller
     {
        $client = new \GuzzleHttp\Client();
        $res = $client->request('GET', 'http://test.localfeedbackloop.com/api?apiKey=61067f81f8cf7e4a1f673cd230216112&noOfReviews=10&internal=1&yelp=1&google=1&offset=50&threshold=1', []); 
-       return response()->json(json_decode($res->body));
+       return response()->json(json_decode($res->getBody()));
     }
 
 }
